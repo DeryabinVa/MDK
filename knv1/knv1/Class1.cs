b4bda72506615992a5fff915fd733914bee7b785
Виталий <72800498+DeryabinVa@users.noreply.github.com>
2020-12-18 08:41:06 +0500

    Add files via upload

---
+++ knv1/knv1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knv1
+{
+    public class Class1
+    {
+        public string loginUser = "user";
+        public string passUser = "0000";
+        
+        public string LoginUser()
+        {
+            return loginUser;
+        }
+        public string PassUser()
+        {
+            return passUser;
+        }
+    }
+}
