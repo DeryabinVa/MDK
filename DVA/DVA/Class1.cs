5a90dcd1bd032128817a3c5107a3ad66dbcba276
Виталий <72800498+DeryabinVa@users.noreply.github.com>
2020-12-21 13:30:48 +0500

    Add files via upload

---
+++ DVA/DVA/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DVA
+{
+   public class Class1
+    {
+        public string passUser = "0000";
+        public string passUser2 = "1111";
+        public string passUser3 = "2222";
+
+        public string PassUser()
+        {
+            return passUser;
+        }
+        public string PassUser2()
+        {
+            return passUser2;
+        }
+        public string PassUser3()
+        {
+            return passUser3;
+        }
+
+
+    }
+}
---
+++ DVA/DVA/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DVA
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            Class1 loginpass = new Class1();
+            Master master = new Master();
+            User user = new User();
+            Slesar slesar = new Slesar();
+            bool error = true;
+
+            if (parol.Password == loginpass.passUser)
+            {
+                master.Show();
+                error = false;
+            }
+
+            if (parol.Password == loginpass.passUser2)
+            {
+                user.Show();
+                error = false;
+
+            }
+
+            if (parol.Password == loginpass.passUser3)
+            {
+                slesar.Show();
+                error = false;
+            }
+            if (error == true)
+            {
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
